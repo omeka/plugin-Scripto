bc44b892af16d36ed51bfdb557dddf572f37411a
Jim Safley <jimsafley@gmail.com>
2011-06-07 01:56:33 -0400

    Modifies the way the transcription and talk pages are toggled.

--- views/shared/index/transcribe.php
+++ views/shared/index/transcribe.php
@@ -9,7 +9,7 @@ jQuery(document).ready(function() {
     
     // Handle edit transcription page.
     jQuery('#scripto-transcription-page-edit').click(function() {
-        jQuery('#scripto-transcription-page-edit').prop('disabled', true).text('Editing...');
+        jQuery('#scripto-transcription-page-edit').prop('disabled', true).text('Editing transcription...');
         jQuery.post(
             <?php echo js_escape(uri('scripto/index/page-action')); ?>, 
             {
@@ -20,7 +20,7 @@ jQuery(document).ready(function() {
                 wikitext: jQuery('#scripto-transcription-page-wikitext').val()
             }, 
             function(data) {
-                jQuery('#scripto-transcription-page-edit').prop('disabled', false).text('Edit');
+                jQuery('#scripto-transcription-page-edit').prop('disabled', false).text('Edit transcription');
                 jQuery('#scripto-transcription-page-html').html(data);
             }
         );
@@ -28,7 +28,7 @@ jQuery(document).ready(function() {
     
     // Handle edit talk page.
     jQuery('#scripto-talk-page-edit').click(function() {
-        jQuery('#scripto-talk-page-edit').prop('disabled', true).text('Editing...');
+        jQuery('#scripto-talk-page-edit').prop('disabled', true).text('Editing discussion...');
         jQuery.post(
             <?php echo js_escape(uri('scripto/index/page-action')); ?>, 
             {
@@ -39,7 +39,7 @@ jQuery(document).ready(function() {
                 wikitext: jQuery('#scripto-talk-page-wikitext').val()
             }, 
             function(data) {
-                jQuery('#scripto-talk-page-edit').prop('disabled', false).text('Edit');
+                jQuery('#scripto-talk-page-edit').prop('disabled', false).text('Edit discussion');
                 jQuery('#scripto-talk-page-html').html(data);
             }
         );
@@ -170,42 +170,37 @@ jQuery(document).ready(function() {
     // Handle default transcription/talk visibility.
     if (window.location.hash == '#discussion') {
         jQuery('#scripto-transcription').hide();
-        jQuery('#scripto-talk-show').css('font-weight', 'bold');
     } else {
         jQuery('#scripto-talk').hide();
-        jQuery('#scripto-transcription-show').css('font-weight', 'bold');
     }
     
-    // Handle show transcription.
-    jQuery('#scripto-transcription-show').click(function(event) {
+    jQuery('#scripto-talk-show').click(function() {
         event.preventDefault();
-        window.location.hash = '#transcription';
-        jQuery('#scripto-talk').hide();
-        jQuery('#scripto-transcription').show();
-        jQuery('#scripto-talk-show').css('font-weight', 'inherit');
-        jQuery('#scripto-transcription-show').css('font-weight', 'bold');
-    });
-    
-    // Handle show talk.
-    jQuery('#scripto-talk-show').click(function(event) {
-        event.preventDefault();
-        window.location.hash = '#discussion';
         jQuery('#scripto-transcription').hide();
         jQuery('#scripto-talk').show();
-        jQuery('#scripto-transcription-show').css('font-weight', 'inherit');
-        jQuery('#scripto-talk-show').css('font-weight', 'bold');
+    });
+    
+    jQuery('#scripto-transcription-show').click(function() {
+        event.preventDefault();
+        jQuery('#scripto-talk').hide();
+        jQuery('#scripto-transcription').show();
     });
     
     // Handle show transcription edit.
-    jQuery('#scripto-transcription-edit-show').click(function() {
-        jQuery(this).hide();
-        jQuery('#scripto-transcription-edit').slideDown();
+    jQuery('#scripto-transcription-edit-show').toggle(function(event) {
+        event.preventDefault();
+        jQuery(this).text('hide edit');
+        jQuery('#scripto-transcription-edit').slideDown('fast');
+    }, function(event) {
+        event.preventDefault();
+        jQuery(this).text('edit');
+        jQuery('#scripto-transcription-edit').slideUp('fast');
     });
     
     // Handle show talk edit.
-    jQuery('#scripto-talk-edit-show').click(function() {
-        jQuery(this).hide();
-        jQuery('#scripto-talk-edit').slideDown();
+    jQuery('#scripto-talk-edit-show').click(function(event) {
+        event.preventDefault();
+        jQuery('#scripto-talk-edit').slideDown('fast');
     });
 });
 </script>
@@ -236,8 +231,6 @@ Logged in as <strong><a href="<?php echo uri('scripto'); ?>"><?php echo $this->s
 <p>
 <?php if (isset($this->paginationUrls['previous'])): ?><a href="<?php echo $this->paginationUrls['previous']; ?>">&#171; previous page</a><?php else: ?>&#171; previous page<?php endif; ?>
  | <?php if (isset($this->paginationUrls['next'])): ?><a href="<?php echo $this->paginationUrls['next']; ?>">next page &#187;</a><?php else: ?>next page &#187;<?php endif; ?>
- | <a id="scripto-transcription-show" href="#transcription">show transcription</a> 
- | <a id="scripto-talk-show" href="#discussion">show discussion</a>
 </p>
 
 <!-- transcription -->
@@ -246,7 +239,7 @@ Logged in as <strong><a href="<?php echo uri('scripto'); ?>"><?php echo $this->s
     <div id="scripto-transcription-edit" style="display: none;">
         <div><?php echo $this->formTextarea('scripto-transcription-page-wikitext', $this->doc->getTranscriptionPageWikitext(), array('cols' => '76', 'rows' => '16')); ?></div>
         <div>
-            <?php echo $this->formButton('scripto-transcription-page-edit', 'Edit', array('style' => 'display:inline; float:none;')); ?> 
+            <?php echo $this->formButton('scripto-transcription-page-edit', 'Edit transcription', array('style' => 'display:inline; float:none;')); ?> 
             <?php if ($this->scripto->isLoggedIn()): ?>
             <?php
             if ($this->doc->isWatchedTranscriptionPage()) {
@@ -278,8 +271,7 @@ Logged in as <strong><a href="<?php echo uri('scripto'); ?>"><?php echo $this->s
     <?php else: ?>
     <p style="color: red;">You don't have permission to transcribe this page.</p>
     <?php endif; ?>
-    <?php echo $this->formButton('scripto-transcription-edit-show', 'Edit transcription', array('style' => 'display:inline; float:none;')); ?>
-    <h2>Current Transcription</h2>
+    <h2>Current Transcription [<a href="#" id="scripto-transcription-edit-show">edit</a>] [<a href="#" id="scripto-talk-show">show discussion</a>]</h2>
     <div id="scripto-transcription-page-html"><?php echo $this->transcriptionPageHtml; ?></div>
 </div><!-- #scripto-transcription -->
 
@@ -289,7 +281,7 @@ Logged in as <strong><a href="<?php echo uri('scripto'); ?>"><?php echo $this->s
     <div id="scripto-talk-edit" style="display: none;">
         <div><?php echo $this->formTextarea('scripto-talk-page-wikitext', $this->doc->getTalkPageWikitext(), array('cols' => '76', 'rows' => '16')); ?></div>
         <div>
-            <?php echo $this->formButton('scripto-talk-page-edit', 'Edit', array('style' => 'display:inline; float:none;')); ?> 
+            <?php echo $this->formButton('scripto-talk-page-edit', 'Edit discussion', array('style' => 'display:inline; float:none;')); ?> 
             <?php if ($this->scripto->canProtect()): ?>
             <?php
             if ($this->doc->isProtectedTalkPage()) {
@@ -308,8 +300,7 @@ Logged in as <strong><a href="<?php echo uri('scripto'); ?>"><?php echo $this->s
     <?php else: ?>
     <p style="color: red;">You don't have permission to discuss this page.</p>
     <?php endif; ?>
-    <?php echo $this->formButton('scripto-talk-edit-show', 'Edit discussion', array('style' => 'display:inline; float:none;')); ?>
-    <h2>Current Discussion</h2>
+    <h2>Current Discussion [<a href="#" id="scripto-talk-edit-show">edit</a>] [<a href="#" id="scripto-transcription-show">show transcription</a>]</h2>
     <div id="scripto-talk-page-html"><?php echo $this->talkPageHtml; ?></div>
 </div><!-- #scripto-talk -->
 
