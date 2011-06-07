b0e5b252160f8e175f0b0c504ce1c5ea4c51e216
Jim Safley <jimsafley@gmail.com>
2011-06-07 17:07:56 -0400

    Updates Scripto library. Fixes user contribution page.

--- views/shared/index/your-contributions.php
+++ views/shared/index/your-contributions.php
@@ -16,17 +16,37 @@ Logged in as <a href="<?php echo uri('scripto'); ?>"><?php echo $this->scripto->
 <table>
     <thead>
     <tr>
-        <th>Document Page</th>
+        <th>Document Page Name</th>
         <th>Contribution Time</th>
         <th>Document Title</th>
     </tr>
     </thead>
     <tbody>
     <?php foreach ($documentPages as $documentPage): ?>
+    <?php
+    // document page name
+    $urlTranscribe = uri(array(
+        'action' => 'transcribe', 
+        'item-id' => $documentPage['document_id'], 
+        'file-id' => $documentPage['document_page_id']
+    ), 'scripto_action_item_file');
+    if (1 == $documentPage['namespace_index']) {
+        $urlTranscribe .= '#discussion';
+    } else {
+        $urlTranscribe .= '#transcription';
+    }
+    
+    // document title
+    $urlItem = uri(array(
+        'controller' => 'items', 
+        'action' => 'show', 
+        'id' => $documentPage['document_id']
+    ), 'id');
+    ?>
     <tr>
-        <td><a href="<?php echo uri(array('action' => 'transcribe', 'doc' => $documentPage['document_id'], 'page' => $documentPage['document_page_id'])); ?>">doc <?php echo $documentPage['document_id']; ?>, page <?php echo $documentPage['document_page_id']; ?></a></td>
-        <td><?php echo gmdate('Y M d, H:i:s', strtotime($documentPage['timestamp'])); ?></td>
-        <td><?php echo $documentPage['document_title']; ?></td>
+        <td><a href="<?php echo $urlTranscribe; ?>"><?php if (1 == $documentPage['namespace_index']): ?>Talk: <?php endif; ?>doc <?php echo $documentPage['document_id']; ?>, page <?php echo $documentPage['document_page_id']; ?></a></td>
+        <td><?php echo gmdate('H:i:s M d, Y', strtotime($documentPage['timestamp'])); ?></td>
+        <td><a href="<?php echo $urlItem; ?>"><?php echo $documentPage['document_title']; ?></a></td>
     </tr>
     <?php endforeach; ?>
     </tbody>
